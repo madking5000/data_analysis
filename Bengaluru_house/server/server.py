e40dac80a43782a0588c817e1fab2de88d408c00
madking <mad.king5005@gmail.com>
2025-05-20 15:30:15 +0200

    updates but i can not finish it hence i do not understand

--- Bengaluru_house/server/server.py
+++ Bengaluru_house/server/server.py
@@ -2,8 +2,11 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/hello')
-def hello():
-    return 'Hi'
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+
 
 if __name__ == '__main__':
     print('starting python flask server for home price prediction...')
